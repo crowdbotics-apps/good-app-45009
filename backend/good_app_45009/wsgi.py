7bc5900bcf6f90434fa4ceccea751c4e783ec749
Crowdbotics <team@crowdbotics.com>
2023-12-14 16:03:50 +0000

    Initial commit

---
+++ backend/good_app_45009/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for good_app_45009 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "good_app_45009.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "good-app-45009",
+  url: "https://good-app-45009.botics.co",
+  api: "https://good-app-45009.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
